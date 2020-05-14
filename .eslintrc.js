6dcee2bf6b0570f3b18f0e825a5aab691aad9cd7
Fanny <fsb62@hotmail.com>
2020-05-14 02:45:18 +0200

    nodemon

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,5 +13,6 @@ module.exports = {
     ecmaVersion: 11,
   },
   rules: {
+    'linebreak-style': 0,
   },
-};
+}
